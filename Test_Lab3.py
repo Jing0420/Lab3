71e4b373a84d7dc1ea3f0148b493d7c5a480bd77
Jing Jing <JingJing.22@ichat.sp.edu.sg>
2023-05-10 20:46:51 +0800

    New changes

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,15 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+def test_bubble_sort_return_integer():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 23, 12, 99, 56]
+    result = Lab3.bubble_sort(input_arr, 2)
+    assert (result == 1)
+def test_bubble_sort_0():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, 1)
+    assert (result == 0)
+def test_bubble_sort_not_integer():
+    input_arr =[64, 34, 25, 12, 22, 11, 90, 60.5, 'a']
+    result=Lab3.bubble_sort(input_arr, 1)
+    assert (result == 2)
